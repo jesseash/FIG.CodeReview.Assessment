f2e0521d34b6e7b6f563379fa38507b1090d8c44
Jesse Ash <45877873+jesseash@users.noreply.github.com>
2022-08-08 09:40:51 -0400

    My review

--- src/FIG.Assessment/Example3.cs
+++ src/FIG.Assessment/Example3.cs
@@ -9,6 +9,26 @@ namespace FIG.Assessment;
 /// users who were created in our system within the last 24 hours, as well as all users who deactivated their account in the last 24 hours. We will then
 /// email this report to the executives so they can monitor how our user base is growing.
 /// </summary>
+
+/*1)Reporting service is registered as singleton class which means it can only be instantiated once. So the dbcontext will be
+the same after 24 hrs for each iteration in the loop. New users will be cached but a user who is deactivated won’t show up
+as being deactivated since EF will throw away the updated data. 
+http://www.referencebits.com/2009/03/entity-framework-patterns-identity-map.html
+
+2)Lines 43 -46 we’re 2 different queries in parallel with one another but will that make it faster? Is faster than
+just running a single for query both deactivated and new users? Can we test it to see if it is? This is an extremely 
+simple query. For a monster query with perhaps 6 other subqueries, I suspect a heavy degree of parallelism is already built into the sql server…
+There is documentation I’ve found which says you can specify degrees of parallelism in each query:
+https://www.sqlshack.com/use-parallel-insert-sql-server-2016-improve-query-performance/
+
+3)Why do we need to store the results in 2 separate variables? It looks like there maybe more overhead involved. 
+Could we return a single datatable with a list of both deactivated and new users, then convert it to an Excel spreadsheet and then e-mail it. Seems like less work. 
+
+4)Why do we need to use a window service, .net, parallel programming, entity framework? Why not just  use the schedule jobs feature on the  sql server?
+https://docs.microsoft.com/en-us/sql/ssms/agent/schedule-a-job?view=sql-server-ver16
+and use a sp to send the e-mail out the execs? Are we not adding extra layers of complexity just by using 
+5 different technologies, when we only need one?
+*/
 public class Example3
 {
     public static void Main(string[] args)
